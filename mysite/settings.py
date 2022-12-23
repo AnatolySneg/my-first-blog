1837c7cd93c6029da5b31fbbe1469e16a1328205
AnatolySneg <AnatolySneg@gmail.com>
2022-12-24 01:56:46 +0200

    Added urls in blog ang HTML for site

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
 ]
+
+CUSTOM_APPS = [
+    'blog'
+]
+
+INSTALLED_APPS += CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
