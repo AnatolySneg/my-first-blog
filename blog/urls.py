082d1abc5938d8cdfc2600a909e576678b2278db
AnatolySneg <AnatolySneg@gmail.com>
2023-01-09 03:40:13 +0200

    created post detail html, new post in progress

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list),
+    path('post/<int:post_pk>/', views.post_detail),
+    path('post/new/', views.post_new),
+    # re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    # re_path(r'^post/new/$', views.post_new),
 ]
